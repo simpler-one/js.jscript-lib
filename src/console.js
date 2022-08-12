c653a36d88a5c75ac8f42d514009b892aabdddec
Simpler one (Koba) <40133262+simpler-one@users.noreply.github.com>
2022-08-12 22:52:25 +0900

    Create console.js

---
+++ src/console.js
@@ -0,0 +1,10 @@
+var Console = {
+    /**
+     * Write message and break line
+     * @param {string} message 
+     * @returns {void}
+     */
+    log: function(message) {
+        WScript.StdOut.WriteLine(message);
+    },
+};
